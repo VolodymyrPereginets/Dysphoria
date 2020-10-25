1d15fba03889d19ba3e4825a124e131329629c2c
VolodymyrPereginets <tsladimir@gmail.com>
2020-10-25 21:47:08 +0200

    Added EB database config and requirements files

--- blog/forms.py
+++ blog/forms.py
@@ -5,35 +5,3 @@ class ContactForm(forms.Form):
     sender = forms.EmailField(widget=forms.TextInput(attrs={'size': '40', 'class': 'form-control'}))
     message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
     copy = forms.BooleanField(required=False)
-
-
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.mail import send_mail, BadHeaderError
-
-
-def contactView(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        # Если форма заполнена корректно, сохраняем все введённые пользователем значения
-        if form.is_valid():
-            subject = form.cleaned_data['subject']
-            sender = form.cleaned_data['sender']
-            message = form.cleaned_data['message']
-            copy = form.cleaned_data['copy']
-
-            recipients = ['ВАШ_EMAIL_ДЛЯ_ПОЛУЧЕНИЯ_СООБЩЕНИЯ']
-            # Если пользователь захотел получить копию себе, добавляем его в список получателей
-            if copy:
-                recipients.append(sender)
-            try:
-                send_mail(subject, message, 'ВАШ_EMAIL_ДЛЯ_ОТПРАВКИ_СООБЩЕНИЯ', recipients)
-            except BadHeaderError:  # Защита от уязвимости
-                return HttpResponse('Invalid header found')
-            # Переходим на другую страницу, если сообщение отправлено
-            return render(request, 'site/thanks.html')
-    else:
-        # Заполняем форму
-        form = ContactForm()
-    # Отправляем форму на страницу
-    return render(request, 'site/contact.html', {'form': form})
